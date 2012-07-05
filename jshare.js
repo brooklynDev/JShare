04b32928c214d259c7184be9d004b4d95ed3d1d1
Alex Friedman <a.friedman07@gmail.com>
2012-07-05 13:40:38 -0400

    Initial Commit

---
+++ jshare.js
@@ -0,0 +1,35 @@
+module.exports = function(){
+	return function(req,res,next){
+		var app = req.app;
+		if(typeof(app) == 'undefined'){
+			var err = new Error("The JShare module requires express");
+			next(err);
+			return;
+		}
+		res.jshare = {};
+		app.dynamicHelpers({
+			includeJShare: function(req,res){
+				return function(namespace){
+					if(typeof(namespace) == 'undefined' || namespace === ''){
+						namespace = "jshare";
+					}
+					if(typeof(res.jshare) === 'undefined' || isEmpty(res.jshare)){
+						return "";
+					}
+					return '<script type="text/javascript">window.' + namespace +'=' + JSON.stringify(res.jshare) + '</script>';
+				}
+			}
+		});
+		next();
+	};
+}
+
+function isEmpty(obj) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
+    return true;
+}
+
+
---
+++ test/jshare.js
@@ -0,0 +1,43 @@
+var expect = require('expect.js');
+var jshare = require('../jshare.js')();
+
+describe('jshare',function(){
+	it('should populate response object with jshare', function(){
+		runJShareTest('jshare',{value : 100}, function(req,res,result){
+			expect(res.jshare).not.to.be(undefined);
+		})
+	})
+	
+	it('should output correct javascript script tag',function(){
+		runJShareTest('jshare',{value : 100}, function(req,res,result){
+			expect(result).to.eql('<script type="text/javascript">window.jshare={"value":100}</script>');
+		})
+	})
+	
+	it('should use custom namespace in client javascript',function(){
+		runJShareTest('customNamespace',{value:100},function(req,res,result){
+			expect(result).to.eql('<script type="text/javascript">window.customNamespace={"value":100}</script>');
+		})
+	})
+	
+	it('should return an empty string if no data was set on server',function(){
+		runJShareTest('jshare',null,function(req,res,result){
+			expect(result).to.be.eql('');
+		})
+	})
+});
+
+function runJShareTest(namespace, data,callback){
+	var app = {};
+	var req = {app : app};
+	var res = {};
+	var next = function(){};
+	app.dynamicHelpers = function(helpers){
+		for(var prop in data){
+			res.jshare[prop] = data[prop];
+		}
+		var result = helpers.includeJShare(req,res)(namespace);
+		callback(req,res,result);
+	}
+	jshare(req,res,next);
+}
