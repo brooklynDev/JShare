0c36b8c611142ec9815ab94425c7092f2d68b1aa
Alex Friedman <a.friedman07@gmail.com>
2012-09-27 00:39:03 -0400

    Remove sample directory

--- sample/routes/index.js
+++
@@ -1,5 +0,0 @@
-exports.index = function(req, res){
-  var person = {firstName: "Alex", lastName: "Friedman", age: 30};
-  res.jshare.person = person;
-  res.render('index', { title: 'Express' })
-};
